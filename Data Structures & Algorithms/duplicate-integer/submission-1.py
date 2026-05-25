fb3056b264f2192bf63e3ab88cc2d8455cc13d4a
blackpearl <monishachakrabarty84@gmail.com>
2026-05-25 16:51:07 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for i in range (len(nums)):
+            if nums[i] in visited:
+                return True
+            visited.add(nums[i])
+            
+        
+        return False
